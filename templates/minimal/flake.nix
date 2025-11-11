c8612ea0a3c79c29ee8be0e6fa1c089b2c73a1fe
oott123 <git@public.oott123.com>
2025-11-11 10:42:41 +0800

    Update nix-on-droid URL to point to the correct repository

--- templates/minimal/flake.nix
+++ templates/minimal/flake.nix
@@ -5,7 +5,7 @@
     nixpkgs.url = "github:NixOS/nixpkgs/nixos-24.05";
 
     nix-on-droid = {
-      url = "github:nix-community/nix-on-droid/release-24.05";
+      url = "github:droidbara/nix-on-droid/master";
       inputs.nixpkgs.follows = "nixpkgs";
     };
   };
